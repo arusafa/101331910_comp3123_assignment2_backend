4a24706bd2ff0979568111a6b6dc73f9d8733652
arusafa <arusafacollege@gmail.com>
2022-12-02 01:17:27 -0500

    all backend files

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express, { json } from "express";
 import employeeRoutes from "./routes/employees";
 import { connect } from "mongoose";
 const app = express()
-const SERVER_PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
+require("dotenv").config();
+const cors = require("cors");
 
 app.use(json())
+app.use(cors())
 
 connect("mongodb+srv://fall2022_comp3123:SAFA.aru1993@cluster0.lclqo7i.mongodb.net/comp3123_assignment2?retryWrites=true&w=majority", {
 
@@ -25,8 +28,8 @@ import userRoutes from "./routes/users";
 //userAPI
 app.use("/api/", userRoutes)
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Server running at http://localhost:${SERVER_PORT}/`)
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server running at http://localhost:${PORT}/`)
     console.log("Press CTRL + C to stop server")
 });
 
